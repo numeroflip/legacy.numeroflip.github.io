4101e43b761bfd333d140c334447e90ea896dff2
numeroflip <berenyi.aron@hotmail.hu>
2020-08-21 11:30:40 +0200

    updating the description content and adding some picture

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,37 +61,7 @@
 
 	// Header.
 
-		// Parallax background.
 
-			// Disable parallax on IE (smooth scrolling is jerky), and on mobile platforms (= better performance).
-				if (browser.name == 'ie'
-				||	browser.mobile)
-					settings.parallax = false;
-
-			if (settings.parallax) {
-
-				breakpoints.on('<=medium', function() {
-
-					$window.off('scroll.strata_parallax');
-					$header.css('background-position', '');
-
-				});
-
-				breakpoints.on('>medium', function() {
-
-					$header.css('background-position', 'left 0px');
-
-					$window.on('scroll.strata_parallax', function() {
-						$header.css('background-position', 'left ' + (-1 * (parseInt($window.scrollTop()) / settings.parallaxFactor)) + 'px');
-					});
-
-				});
-
-				$window.on('load', function() {
-					$window.triggerHandler('scroll');
-				});
-
-			}
 
 	// Main Sections: Two.
 
